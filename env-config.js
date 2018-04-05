c38cc8e675bdb347ee20ccffb4356c38b685b9c3
hrdtbs <harada.tsubasa.0422+eiminsasete@gmail.com>
2018-04-06 00:47:00 +0900

    update

--- env-config.js
+++ env-config.js
@@ -1,5 +1,7 @@
 const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
-  "process.env.BACKEND_URL": prod ? "/responsive-web-design" : ""
+  "process.env.BACKEND_URL": prod
+    ? "https://hrdtbs.github.io/responsive-web-design"
+    : ""
 };
