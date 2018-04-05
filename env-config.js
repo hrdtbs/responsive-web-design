cfa351ea368705136d54afff862a8177dbba65af
hrdtbs <harada.tsubasa.0422+eiminsasete@gmail.com>
2018-04-06 00:49:42 +0900

    fix

--- env-config.js
+++ env-config.js
@@ -1,7 +1,5 @@
 const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
-  "process.env.BACKEND_URL": prod
-    ? "https://hrdtbs.github.io/responsive-web-design"
-    : ""
+  "process.env.BACKEND_URL": prod ? "/responsive-web-design" : ""
 };
